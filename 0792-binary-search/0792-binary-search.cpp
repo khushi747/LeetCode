cf9d11b56e4b05273cd88a29bc11c7730c21f825
Khushi Shukla <95963491+khushi747@users.noreply.github.com>
2024-05-29 22:16:53 +0530

    Time: 22 ms (86.63%), Space: 30 MB (22.06%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int size=nums.size();
+        int start=0;
+        int end=size-1;
+        int mid=(start+end)/2;
+
+        while(start<=end){
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+            mid=(start+end)/2;
+        }
+        return -1;
+    }
+};
