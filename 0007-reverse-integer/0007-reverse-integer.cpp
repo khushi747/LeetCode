91b24576e35f8c0e4390fb32428be349df39fa3b
Khushi Shukla <95963491+khushi747@users.noreply.github.com>
2024-07-01 23:33:33 +0530

    Time: 3 ms (24.78%), Space: 7.3 MB (87.03%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -9,6 +9,8 @@ public:
             revInt=(revInt*10)+rem;
             x=x/10;
         }
+
+        //Consider Boundaries, which i initially forgot and got errors
         if(revInt>INT_MAX || revInt<INT_MIN) return 0; 
         return revInt;
     }
