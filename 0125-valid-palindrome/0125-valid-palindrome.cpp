087c835a037ef53bd3b12051e470d7530f117230
Khushi Shukla <95963491+khushi747@users.noreply.github.com>
2024-07-02 22:57:39 +0530

    Time: 4 ms (79.71%), Space: 8.6 MB (83.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -8,18 +8,18 @@ public:
            if(!isalnum(s[start])){
             start++; 
             continue;
-        }
+            }
            if(!isalnum(s[end])){
             end--;
             continue;
             }
            if(tolower(s[start])!=tolower(s[end])){
             return false;
-           }
+            }
            else{
                start++;
                end--;
-           }
+            }
        }
        return true;
 }
