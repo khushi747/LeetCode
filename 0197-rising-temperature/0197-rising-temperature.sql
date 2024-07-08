2eca6d5d372345de6373ecfa13e5dc2281a3845d
Khushi Shukla <95963491+khushi747@users.noreply.github.com>
2024-07-08 12:27:39 +0530

    Time: 712 ms (52.86%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,5 @@
-/* Write your T-SQL query statement below */
-SELECT b.id as id
-FROM Weather a
-INNER JOIN Weather b
-ON datediff(day,a.recordDate,b.recordDate) = 1
-AND a.temperature < b.temperature;
+SELECT b.id 
+FROM Weather a 
+JOIN Weather b ON datediff(day,a.recordDate,b.recordDate)=1 
+WHERE b.temperature-a.temperature>0;  /* Write your T-SQL query statement below */
+
