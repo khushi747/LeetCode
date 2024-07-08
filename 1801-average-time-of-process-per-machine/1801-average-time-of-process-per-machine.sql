39a36fedd97cdb777b1fc16ecccfd7b99e4e1d10
Khushi Shukla <95963491+khushi747@users.noreply.github.com>
2024-07-08 14:24:40 +0530

    Time: 225 ms (63.7%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
+
+-- Basically we are merging end time with start time in a single table then finding the differece between two times which are present in same row- a2.timestamp-a1.timestamp
+
 SELECT a1.machine_id, round(avg(a2.timestamp-a1.timestamp),3) AS processing_time
 FROM activity a1
 JOIN activity a2 ON a1.machine_id=a2.machine_id AND a1.process_id=a2.process_id AND a1.activity_type='start' AND a2.activity_type='end'
