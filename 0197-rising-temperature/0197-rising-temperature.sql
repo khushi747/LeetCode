d86a06295ee9e63aba1a67288987720d93deef67
Khushi Shukla <95963491+khushi747@users.noreply.github.com>
2024-07-09 12:10:48 +0530

    Time: 697 ms (52.35%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,8 @@
-SELECT b.id 
-FROM Weather a 
-JOIN Weather b ON datediff(day,a.recordDate,b.recordDate)=1 
-WHERE b.temperature-a.temperature>0;  /* Write your T-SQL query statement below */
+-- Actually here we are joining two same tables of weather on a condition where b ki recorddate a se ek din ki aage ki ho aur fir us row ki dono temperature (that is temperature of both of these consecutive days) ka difference agar o se bada ho, then that row is in our answer 
+
+SELECT b.id
+FROM weather a 
+JOIN weather b ON datediff(day, a.recorddate,b.recorddate)=1 
+WHERE b.temperature-a.temperature>0;
+
 
