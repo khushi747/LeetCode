e441332f7afa5b771c4d277056301585a0437afd
Khushi Shukla <95963491+khushi747@users.noreply.github.com>
2024-07-11 14:50:39 +0530

    Time: 5 ms (21.05%), Space: 8.3 MB (5.83%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int reverse(int x) {
-        long revInt=0;
+        long long revInt=0;
         int rem=0;
-
         while(x!=0){
             rem=x%10;
-            revInt=(revInt*10)+rem;
+            revInt=revInt*10+rem;
             x=x/10;
         }
-
+        
         //Consider Boundaries, which i initially forgot and got errors
         if(revInt>INT_MAX || revInt<INT_MIN) return 0; 
+
         return revInt;
     }
 };
+
+ 
+
