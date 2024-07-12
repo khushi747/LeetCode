247f5a9da07b3034b64afbfbfc4e43b7e6244b22
Khushi Shukla <95963491+khushi747@users.noreply.github.com>
2024-07-12 20:12:31 +0530

    Time: 4 ms (84.18%), Space: 8.4 MB (27.05%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x){
-    if(x<0 || x!=0 && x%10 ==0 ) return false;
-    int check=0;
-    while(x>check){
-        check = check*10 + x%10;
-        x/=10;
-    }
-    return (x==check || x==check/10);
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+
+        int k=x;
+        long long t=0;
+        while(x!=0){
+            t=t*10+x%10;
+            x=x/10;
+        }
+        return t==k;
+
     }
 };
