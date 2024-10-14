508d8b8132504ca8feb4923790d577c5d2b25cf0
Khushi Shukla <95963491+khushi747@users.noreply.github.com>
2024-10-14 21:58:57 +0530

    Time: 12 ms (66.08%), Space: 19.6 MB (66.07%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int unique=0;
+        int ans=0;
         for(int i=0;i<nums.size();i++){
-            unique=unique^nums[i];
+            ans=ans^nums[i];
         }
-        return unique;
+        return ans;
     }
 };
